66fcfe824e01a7f7a87e0c8137780bc5fdb2a18a
lindliu <dliu2020@outlook.com>
2023-03-29 12:08:56 +0200

    d

--- Alexandros/main.py
+++ Alexandros/main.py
@@ -283,7 +283,7 @@ if __name__ == '__main__':
         data = pd.DataFrame(np.load('../data/simulation_2_3.npy'), columns=['S','I','R'])            
     
     dis = 3
-    for num in range(100,300,dis):
+    for num in range(20,300,dis):
         ##### data preparation ######
         length = 400
         recovery_time = 14
