09bd48139dfb5b34d816b055624a297b9efb765d
lindliu <dliu2020@outlook.com>
2023-04-03 14:45:32 +0200

    d

--- odeint_BetaVar_tauVar_activation/train_odeint _estimated_series_.py
+++ odeint_BetaVar_tauVar_activation/train_odeint _estimated_series_.py
@@ -305,7 +305,7 @@ if __name__ == '__main__':
     countries = ['simulation', 'Mexico', 'South Africa', 'Republic of Korea',\
                  'Belgium', 'United Kingdom', 'Slovenia', 'Denmark']
     
-    country = countries[2]
+    country = countries[3]
     
     ### set estimate=false if using real cases to train
     # estimate, prop = True, True 
@@ -320,7 +320,7 @@ if __name__ == '__main__':
         data = pd.DataFrame(np.load('../data/simulation_2_3.npy'), columns=['S','I','R'])            
     
     dis = 6
-    for num in range(158,300,dis):
+    for num in range(182,300,dis):
     # for num in np.arange(170,300,12):
         ##### data preparation ######
         length = 400
@@ -445,8 +445,8 @@ if __name__ == '__main__':
                     # if loss<3e-4: ## simulation
                     # if loss<1e-5: ## estimated mexico and south korea
                     # if loss<2e-4: ## 2e-5 # estimated south africa 
-                    if loss<1e+7: ###real south africa
-                    # if loss<5e+7: ###real south korea
+                    # if loss<1e+7: ###real south africa
+                    if loss<5e+7: ###real south korea
                         flag = True
                         break
                     try:
